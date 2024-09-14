81c941a36d857f81ecd4eab1f5bff8a9c7b1be6c
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-09-14 20:09:09 +0700

    Membuat element button

---
+++ frontend/src/components/elements/button/Button.jsx
@@ -0,0 +1,32 @@
+// menggunakan (Props) / properties
+// didefinsikan sebagai sebuah varameter funtion
+const Button = (props) => {
+  // konsep desctrukturing
+  const {
+    children,
+    className = "bg-black",
+    onClick = () => {},
+    type = "button",
+  } = props;
+  return (
+    <button
+      // className={`h-10 px-6 font-semibold rounded-md ${props.variant} text-black`}
+      // konsep desctrukturing
+      className={`h-10 px-6 font-semibold rounded-md ${className} text-white`}
+      type={type}
+      // event handler
+      onClick={onClick}
+    >
+      {/* {props.text} */}
+
+      {/* konsep desctrukturing */}
+      {children}
+
+      {/* props spesial yaitu (children) */}
+      {/* sebuah komponen yang bisa menangkap apa yang ada di dalam tag componen tersbut */}
+      {/* {props.children} */}
+    </button>
+  );
+};
+
+export default Button;
