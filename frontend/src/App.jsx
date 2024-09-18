5b93bbc6a877f126bb6cdce16a4c4988f0d1d5e8
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-09-18 19:53:51 +0700

    Membuat route yang nantinya tempat kontrol semua route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,37 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+import LoginPage from "./pages/Login.jsx";
+import DashboardPage from "./pages/Dashboard.jsx";
+import DataPemesananPage from "./pages/DataPemesanan.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    // element: <div>Hello World</div>,
+    element: <Navigate to="/login" />, // Redirect ke /login ketika mengakses "/"
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardPage />,
+  },
+  {
+    path: "/data-pemesanan",
+    element: <DataPemesananPage />,
+  },
+]);
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Bismillah Yaa!</h1>
-    </>
+    // <div className="flex justify-center min-h-screen items-center">
+    <RouterProvider router={router} />
+    // </div>
   );
 }
 
