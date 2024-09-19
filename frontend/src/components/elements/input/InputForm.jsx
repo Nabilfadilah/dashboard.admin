8b970043f6a18627d75e9d8fca285135cfa76c21
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-09-19 16:08:39 +0700

    Merubah element input form untuk menyesuaikan menggunakan formik

--- frontend/src/components/elements/input/InputForm.jsx
+++ frontend/src/components/elements/input/InputForm.jsx
@@ -1,20 +1,101 @@
+// import { forwardRef } from "react";
+// import Input from "./Input";
+// import Label from "./Label";
+
+// const InputForm = forwardRef((props, ref) => {
+//   const { label, name, type, placeholder, className } = props; // Ambil className dari props
+
+//   return (
+//     <div className="mb-3">
+//       <Label htmlFor={name}>{label}</Label>
+//       <Input
+//         name={name}
+//         type={type}
+//         placeholder={placeholder}
+//         ref={ref}
+//         className={className} // Gunakan className yang diterima dari props
+//       />
+//     </div>
+//   );
+// });
+
+// export default InputForm;
+
+// opsi 2
+// import { forwardRef } from "react";
+// import Input from "./Input";
+// import Label from "./Label";
+
+// const InputForm = forwardRef((props, ref) => {
+//   const {
+//     label,
+//     name,
+//     type,
+//     placeholder,
+//     className,
+//     onChange,
+//     onBlur,
+//     value,
+//     error,
+//   } = props;
+
+//   return (
+//     <div className="mb-2">
+//       {" "}
+//       {/* Mengatur jarak vertikal */}
+//       <Label htmlFor={name}>{label}</Label>
+//       <Input
+//         name={name}
+//         type={type}
+//         placeholder={placeholder}
+//         ref={ref}
+//         className={`${className} ${
+//           error ? "border-red-500" : "border-gray-300"
+//         }`} // Tambahkan kondisi border merah
+//         onChange={onChange}
+//         onBlur={onBlur}
+//         value={value}
+//       />
+//     </div>
+//   );
+// });
+
+// export default InputForm;
+
+// opsi 3
 import { forwardRef } from "react";
 import Input from "./Input";
 import Label from "./Label";
 
 const InputForm = forwardRef((props, ref) => {
-  // ini mengambil dari semua props di input dan label
-  const { label, name, type, placeholder } = props;
+  const {
+    label,
+    name,
+    type,
+    placeholder,
+    className,
+    onChange,
+    onBlur,
+    value,
+    error,
+  } = props;
 
   return (
-    <div className="mb-6">
+    <div className="mb-3 w-full">
+      {" "}
+      {/* Tambahkan w-full */}
       <Label htmlFor={name}>{label}</Label>
       <Input
         name={name}
         type={type}
         placeholder={placeholder}
         ref={ref}
-      ></Input>
+        className={`w-full ${className}`} // Pastikan w-full ditambahkan
+        onChange={onChange}
+        onBlur={onBlur}
+        value={value}
+        error={error}
+      />
     </div>
   );
 });
